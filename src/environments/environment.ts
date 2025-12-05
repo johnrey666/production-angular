f3d5177bd4924b83513dc00a16278ce90c6433b1
Janrey <johnreydado3@gmail.com>
2025-12-05 17:26:49 +0800

    pwede na muna

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://fgmkzhyuuljeyoiulwni.supabase.co', // From Step 1
+  // During local development we proxy requests through the dev server to avoid CORS.
+  // The proxy rewrites `/supabase` -> https://fgmkzhyuuljeyoiulwni.supabase.co
+  supabaseUrl: 'https://fgmkzhyuuljeyoiulwni.supabase.co',
   supabaseKey: 'sb_publishable_18oP9_5w69SUxbmmFm7PZw_LtKlT_Vs' // From Step 1
 };
